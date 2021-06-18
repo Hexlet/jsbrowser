c3b74ea11673f1189765b0aecf8fb62ba670c145
Alexander Sergeev <aleksanteri.sergeev@gmail.com>
2021-06-18 16:47:16 +0300

    added lints and actions

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,5 +1,6 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './App.jsx';
 
 test('renders learn react link', () => {
   render(<App />);
