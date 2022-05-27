f12dde5d942665df6571df3c12a06c9e835097cd
ola-9 <nepogodda@gmail.com>
2022-05-27 18:45:32 +0300

    update tests to account for changes

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,29 +1,17 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
+import store from './store.js';
 import App from './App.jsx';
 
-describe('With React Testing Library', () => {
-  const defaultCurrentTab = '1';
-  const initialState = {
-    tabs: {
-      tabsList: [
-        {
-          id: defaultCurrentTab,
-          name: 'New Tab',
-        },
-      ],
-      currentTabId: defaultCurrentTab,
-    },
-  };
+test('Work', async () => {
+  const vdom = (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  );
 
-  const mockStore = configureStore();
-  let store; // eslint-disable-line
+  render(vdom);
 
-  it('Shows "New Tab"', () => {
-    store = mockStore(initialState);
-    const { getByText } = render(<Provider store={store}><App /></Provider>);
-    expect(getByText('New Tab')).not.toBeNull();
-  });
+  expect(await screen.findByText('New Tab')).toBeInTheDocument();
 });
