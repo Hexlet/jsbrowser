3579279c0c3409bc69cf773cf89b9c7f221806ee
Evgeniy Sedov <sedoff-online@yandex.ru>
2023-05-27 11:41:36 +0700

    chenges search from yandex on bing

--- src/components/IframeTemplate.jsx
+++ src/components/IframeTemplate.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 
-const IframeTemplate = ({ tabData }) => (
-  <iframe srcDoc={tabData} title="to be titled" width="100%" height="100%" />
+const IframeTemplate = ({
+  // tabData,
+  link,
+}) => (
+  <iframe
+    // srcDoc={tabData}
+    src={link}
+    title="to be titled"
+    width="100%"
+    height="100%"
+  />
 
 );
 
